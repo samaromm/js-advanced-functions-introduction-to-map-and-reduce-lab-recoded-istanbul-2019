dcf545405173666747af541c74c378ea998d9808
Samar Omar <samaromm98@gmail.com>
2020-02-03 17:49:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,11 @@ function mapToNNoChange(arr){
   let arr2=[...arr]
   return arr2
 }
+
+function mapToNegativize(arr){
+  let arr2=[]
+  for(let ele of arr){
+    arr2.push(ele*2)
+  }
+  return arr2
+}
